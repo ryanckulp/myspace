3cea80c1c6f8150db007b753ae66521619b29c64
Ryan Kulp <ryanckulp@gmail.com>
2015-09-28 05:21:36 +0700

    fb url

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ActiveRecord::Base
     end
 
     def self.create_user_from_omniauth(auth)
+      binding.pry
       create(
         provider: auth['provider'],
         uid: auth['uid'],
         name: auth['info']['name'],
         avatar: auth['info']["image"],
+        fb_url: auth["info"]["urls"]["Facebook"],
         email: auth['extra']['raw_info']['email'],
         fb_friends: auth['extra']['raw_info']['friends']['summary']['total_count']
         )
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
   provider :facebook, ENV['FB_APP'], ENV['FB_SECRET'], 
-  :info_fields => 'name, email, friends,relationship_status', :scope => 'public_profile, email, user_friends'
+  :info_fields => 'name, email, friends,relationship_status, link', :scope => 'public_profile, email, user_friends'
 end
