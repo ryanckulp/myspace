122896ccd09ecfdcb0afd0156ab884597fa9cd38
Ryan Kulp <ryanckulp@gmail.com>
2015-09-28 04:35:15 +0700

    remove more perms

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
   provider :facebook, ENV['FB_APP'], ENV['FB_SECRET'], 
-  :info_fields => 'name, email, friends, picture, photos, birthday, first_name, last_name, link', 
+  :info_fields => 'name, email, friends, picture, first_name, last_name, link', 
   :scope => 'public_profile, email, user_friends'
 end
