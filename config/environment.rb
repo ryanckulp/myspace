55df54da56ee5135315e88daccf9efcd5cd37282
Ryan Kulp <ryanckulp@gmail.com>
2015-09-28 02:59:33 +0700

    debug on prod

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# debug on Heroku
+ActiveRecord::Base.logger.level = Logger::DEBUG
