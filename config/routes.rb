6db9f7d183d5edb3c91140d0e0d920441d7f5794
Ryan Kulp <ryanckulp@gmail.com>
2015-09-28 05:10:53 +0700

    route cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   root "home#index"
   get 'profile', to: 'home#profile', as: 'profile'
-  get 'auth/facebook', as: 'auth'
   get 'auth/:provider/callback', to: "sessions#create"
   delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
 
