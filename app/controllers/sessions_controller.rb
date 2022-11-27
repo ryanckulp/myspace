07e75ed71f36e4b4d961762b0d286e5211b5874d
Ryan Kulp <ryanckulp@gmail.com>
2022-11-27 14:09:31 -0500

    exmaple for posterity

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,17 @@
 class SessionsController < ApplicationController
 
   def create
-      auth = request.env["omniauth.auth"]
-      session[:omniauth] = auth.except('extra')
-      user = User.sign_in_from_omniauth(auth)
-      session[:user_id] = user.id
-      redirect_to profile_path, notice: "SIGNED IN"
+    auth = request.env["omniauth.auth"]
+    session[:omniauth] = auth.except('extra')
+    user = User.sign_in_from_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to profile_path, notice: "SIGNED IN"
+  end
+
+  def example
+    user = User.find_by(name: 'Ryan Kulp')
+    session[:user_id] = user.id
+    redirect_to profile_path
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'profile', to: 'home#profile', as: 'profile'
   get 'auth/:provider/callback', to: "sessions#create"
   delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
+
+  get 'example', to: 'sessions#example'
 end
